58f5b596217a085dfae98b29461f5d6669121177
DanieleIsoni <daniele.isoni@studenti.unitn.it>
2020-05-21 19:38:44 +0200

    code formatting and docs added for alignment.py

--- alignment.py
+++ alignment.py
@@ -1,12 +1,47 @@
 from operator import attrgetter
+from typing import Any, Iterator, List
+
 from colorama import Fore, Style
 
+
 class Alignment:
-    def __init__(self, subseq1, subseq2, max_gap, min_gap, n_gaps, score, indexes):
+    """Represents an alignment.
+
+    Attributes:
+        subseq1 (str): The first subsequence of the alignment
+        subseq2 (str): The second subsequence of the alignment
+        length (int): The length of the alignment
+        max_gap (int): The size of the biggest gap found in the alignment
+        min_gap (int): The size of the smaller gap found in the alignment
+        n_gaps (int): The number of gaps found in the alignment
+        score (float): The score of the alignment
+        indices ((int, int)): The starting indices for the traceback of the alignment
+    """
+
+    def __init__(
+        self,
+        subseq1: str,
+        subseq2: str,
+        max_gap: int,
+        min_gap: int,
+        n_gaps: int,
+        score: float,
+        indices: (int, int),
+    ) -> None:
+        """
+        Args:
+        subseq1 (str): The first subsequence of the alignment
+        subseq2 (str): The second subsequence of the alignment
+        max_gap (int): The size of the biggest gap found in the alignment
+        min_gap (int): The size of the smaller gap found in the alignment
+        n_gaps (int): The number of gaps found in the alignment
+        score (float): The score of the alignment
+        indices ((int, int)): The starting indices for the traceback of the alignment
+        """
 
         if len(subseq1) != len(subseq2):
             raise ValueError("Something went wrong, the two subsequences are not the same length")
-        
+
         self.subseq1 = subseq1
         self.subseq2 = subseq2
         self.length = len(subseq1)
@@ -14,11 +49,12 @@ class Alignment:
         self.min_gap = min_gap
         self.n_gaps = n_gaps
         self.score = score
-        self.indexes = indexes
-    
-    def _colored_subsequences(self):
-        string1 = ''
-        string2 = ''
+        self.indices = indices
+
+    def _colored_subsequences(self) -> str:
+        """This method is needed to provied a colored output to the console"""
+        string1 = ""
+        string2 = ""
         for c1, c2 in zip(self.subseq1, self.subseq2):
             if c1 == c2:
                 string1 += Fore.GREEN + c1
@@ -26,97 +62,147 @@ class Alignment:
             else:
                 string1 += Fore.RED + c1
                 string2 += Fore.RED + c2
-        return f'{string1}\n{string2}{Style.RESET_ALL}'
+        return f"{string1}\n{string2}{Style.RESET_ALL}"
 
-    def to_string(self, to_file=False):
-        alignment_string = f'score: {self.score}\n'
+    def to_string(self, to_file: bool = False) -> str:
+        """This is needed to provied an output string that is colored if not to be printed to file.
+
+        Args:
+            to_file (bool): if True the output will not be colored.
+        """
+
+        alignment_string = f"score: {self.score}\n"
         if to_file:
-            alignment_string += self.subseq1 + '\n' + self.subseq2
+            alignment_string += self.subseq1 + "\n" + self.subseq2
         else:
             alignment_string += self._colored_subsequences()
-        
-        return f'''{alignment_string}
+
+        return f"""{alignment_string}
 max_gap: {self.max_gap}
 min_gap: {self.min_gap}
 n_gaps: {self.n_gaps}
-trace_back_start_indexes: {self.indexes}
-'''
+trace_back_start_indices: {self.indices}
+"""
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.to_string()
 
 
 class Alignments(object):
-    filter_props = {'length', 'max_gap', 'min_gap', 'n_gaps', 'score'}
-    filter_operators = {'neq', 'gt', 'gte', 'lt', 'lte'}
+    """Represents a list of alignments
 
-    def __init__(self, alignments = []):
+    This has been created to have a structure to manage better the filtering of the alignments found.
+
+    Attributes:
+        filter_props (:obj:`dict` of str): The props allowed for filtering the alignments.
+        filter_operators (:obj:`dict` of str): The operators allowed for filtering the alignments.
+
+        alignments (:obj:`list` of Alignment): This is the actual list of alignments
+    """
+
+    filter_props = {"length", "max_gap", "min_gap", "n_gaps", "score"}
+    filter_operators = {"neq", "gt", "gte", "lt", "lte"}
+
+    def __init__(self, alignments: List[Alignment] = []) -> None:
         if any(not isinstance(a, Alignment) for a in alignments):
             raise TypeError("One or more of the objects is not an Alignment object.")
 
         self._alignments = alignments
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[Alignment]:
+        """This allows to iterate through this object"""
+
         for alignment in self._alignments:
             yield alignment
 
-    def __str__(self):
+    def __str__(self) -> str:
         return str([str(al) for al in self._alignments])
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self._alignments)
 
-    def sort(self, key, reverse=False):
+    def sort(self, key: str, reverse: bool = False) -> None:
+        """Used to sort inplace the alignment list"""
+
         self._alignments.sort(key=attrgetter(key), reverse=reverse)
 
-    def append(self, alignment):
+    def append(self, alignment: Alignment) -> None:
+        """Used to add to the end of the list a new alignment"""
+
         if not isinstance(alignment, Alignment):
             raise TypeError("One or more of the objects is not an Alignment object.")
 
         self._alignments.append(alignment)
 
-    def _get_filter_option(self, kwarg):
-        kwarg_list = kwarg.split('__')
-        
+    def _get_filter_option(self, kwarg: str) -> (str, str):
+        """Given an argument provides the property and the operator to be used to filter.
+
+        Args:
+            kwarg (str): The argument provided in the form property__operator  (e.g.: length__neq, n_gaps__gt, max_gap)
+
+        Returns:
+            A tuple containing the property and the operator if both are valid, otherwise raises an exception.
+        """
+
+        kwarg_list = kwarg.split("__")
+
         if len(kwarg_list) > 2:
-            raise TypeError(f'filter() got an unexpected keyword argument {kwarg}.')
+            raise TypeError(f"filter() got an unexpected keyword argument {kwarg}.")
 
         prop = kwarg_list[0]
         operator = kwarg_list[1] if len(kwarg_list) > 1 else None
 
         if prop not in self.filter_props:
-            raise TypeError(f"{prop} is not a valid filter() properties. Valid properties are: {','.join(self.filter_props)}")
+            raise TypeError(
+                f"{prop} is not a valid filter() properties. Valid properties are: {','.join(self.filter_props)}"
+            )
 
         if operator and operator not in self.filter_operators:
-            raise TypeError(f"{operator} is not a valid filter() operator. Valid operators are: {','.join(self.filter_operators)}")
-        
+            raise TypeError(
+                f"{operator} is not a valid filter() operator. Valid operators are: {','.join(self.filter_operators)}"
+            )
+
         return prop, operator
 
-    def _compute_expression(self, alignment, query, value):
+    def _compute_expression(self, alignment: Alignment, query: (str, str), value: Any) -> bool:
+        """This method computes the expression provided on the given alignment
+
+        Args:
+            alignment (:obj:`Alignment`): The alignemnt to be tested
+            query ((str, str)): A tuple of strings which respectively are the property and
+                the operator that need to be applied to the alignment
+
+        Returns:
+            A boolean that indicates if the alignment is to be kept (True) or filtered (False)
+        """
+
         prop, operator = self._get_filter_option(query)
 
         if not operator:
             return getattr(alignment, prop) == value
-        elif operator == 'neq':
+        elif operator == "neq":
             return getattr(alignment, prop) != value
-        elif operator == 'gt':
+        elif operator == "gt":
             return getattr(alignment, prop) > value
-        elif operator == 'gte':
+        elif operator == "gte":
             return getattr(alignment, prop) >= value
-        elif operator == 'lt':
+        elif operator == "lt":
             return getattr(alignment, prop) < value
-        elif operator == 'lte':
+        elif operator == "lte":
             return getattr(alignment, prop) <= value
-            
 
-    def filter(self, **kwargs):
+    def filter(self, **kwargs: dict) -> "Alignments":
+        """The actual method used to filter the alignments"""
+
         filtered_alignments = []
         for alignment in self._alignments:
             is_alignment_wanted = True
             for key, value in kwargs.items():
-                is_alignment_wanted = is_alignment_wanted and self._compute_expression(alignment, key, value)
+                is_alignment_wanted = is_alignment_wanted and self._compute_expression(
+                    alignment, key, value
+                )
 
             if is_alignment_wanted:
                 filtered_alignments.append(alignment)
-        
+
         return Alignments(filtered_alignments)
--- smith_waterman.py
+++ smith_waterman.py
@@ -1,47 +1,54 @@
 import argparse
-from colorama import Fore, Style
+import os
+import sys
 from enum import Enum
 from itertools import chain
+from typing import List
+
 import numpy as np
-import os
 import pandas as pd
-import sys
+from colorama import Fore, Style
 
 from alignment import Alignment, Alignments
 
+
 class Move(Enum):
-    HORIZONTAL = '\u2190'
-    VERTICAL = '\u2191'
-    DIAGONAL = '\u2196'
+    HORIZONTAL = "\u2190"
+    VERTICAL = "\u2191"
+    DIAGONAL = "\u2196"
+
 
 class Cell:
-    def __init__(self, score, indices, origin):
+    def __init__(self, score: float, indices: (int, int), origin: Move) -> None:
         self.score = score
         self.indices = indices
         self.origin = origin
 
-    def __str__(self):
-        return f'{self.origin.value} {self.score}'
+    def __str__(self) -> str:
+        return f"{self.origin.value} {self.score}"
 
-    def __eq__(self, other): 
-        return self.score == other.score 
+    def __eq__(self, other: "Cell") -> bool:
+        return self.score == other.score
 
-    def __ne__(self, other): 
+    def __ne__(self, other: "Cell") -> bool:
         return not self.__eq__
 
-    def __lt__(self, other): 
-        return self.score < other.score 
-     
-    def __le__(self, other):
+    def __lt__(self, other: "Cell") -> bool:
+        return self.score < other.score
+
+    def __le__(self, other: "Cell") -> bool:
         return self.score <= other.score
 
-    def __gt__(self, other): 
-        return self.score > other.score 
-    
-    def __ge__(self, other):
-        return self.score >= other.score   
+    def __gt__(self, other: "Cell") -> bool:
+        return self.score > other.score
 
-def compute_scoring_matrix(seq1, seq2, match_score, mismatch_score, gap_penalty):
+    def __ge__(self, other: "Cell") -> bool:
+        return self.score >= other.score
+
+
+def compute_scoring_matrix(
+    seq1: str, seq2: str, match_score: float, mismatch_score: float, gap_penalty: float
+) -> List[List[Cell]]:
     n = len(seq1) + 1
     m = len(seq2) + 1
 
@@ -49,19 +56,27 @@ def compute_scoring_matrix(seq1, seq2, match_score, mismatch_score, gap_penalty)
 
     for i in range(1, n):
         for j in range(1, m):
-            seq_i = seq1[i-1]
-            seq_j = seq2[j-1]
+            seq_i = seq1[i - 1]
+            seq_j = seq2[j - 1]
 
-            match = Cell(scoring_matrix[i - 1][j - 1].score + (match_score if seq_i == seq_j else mismatch_score), (i,j), Move.DIAGONAL)
-            h_gap = Cell(scoring_matrix[i][j - 1].score + gap_penalty, (i,j), Move.HORIZONTAL)
-            v_gap = Cell(scoring_matrix[i - 1][j].score + gap_penalty, (i,j), Move.VERTICAL)
+            match = Cell(
+                scoring_matrix[i - 1][j - 1].score
+                + (match_score if seq_i == seq_j else mismatch_score),
+                (i, j),
+                Move.DIAGONAL,
+            )
+            h_gap = Cell(scoring_matrix[i][j - 1].score + gap_penalty, (i, j), Move.HORIZONTAL)
+            v_gap = Cell(scoring_matrix[i - 1][j].score + gap_penalty, (i, j), Move.VERTICAL)
 
             scoring_matrix[i][j] = max(match, h_gap, v_gap, Cell(0, (i, j), Move.DIAGONAL))
 
     return scoring_matrix
 
-def traceback_process(scoring_matrix, seq1, seq2, starting_cell):
-    subseq1, subseq2 = '', ''
+
+def traceback_process(
+    scoring_matrix: List[List[Cell]], seq1: str, seq2: str, starting_cell: Cell
+) -> Alignment:
+    subseq1, subseq2 = "", ""
     max_gap = 0
     min_gap = max(len(seq1), len(seq2))
     n_gaps = 0
@@ -72,7 +87,7 @@ def traceback_process(scoring_matrix, seq1, seq2, starting_cell):
         i, j = actual_cell.indices
         seq_i = seq1[i - 1]
         seq_j = seq2[j - 1]
-        
+
         if actual_cell.origin == Move.DIAGONAL:
             if tmp_gap:
                 max_gap = max(max_gap, tmp_gap)
@@ -82,57 +97,86 @@ def traceback_process(scoring_matrix, seq1, seq2, starting_cell):
 
             subseq1 += seq_i
             subseq2 += seq_j
-            actual_cell = scoring_matrix[i-1][j-1]
+            actual_cell = scoring_matrix[i - 1][j - 1]
         else:
-            tmp_gap = tmp_gap+1 if tmp_gap else 1
+            tmp_gap = tmp_gap + 1 if tmp_gap else 1
             if actual_cell.origin == Move.HORIZONTAL:
-                subseq1 += '-'
+                subseq1 += "-"
                 subseq2 += seq_j
-                actual_cell = scoring_matrix[i][j-1]
+                actual_cell = scoring_matrix[i][j - 1]
             elif actual_cell.origin == Move.VERTICAL:
                 subseq1 += seq_i
-                subseq2 += '-'
-                actual_cell = scoring_matrix[i-1][j]
+                subseq2 += "-"
+                actual_cell = scoring_matrix[i - 1][j]
             else:
-                raise Exception(f'Something went wrong origin must be one of {",".join([move for move in Move])}')
-        
+                raise Exception(
+                    f'Something went wrong origin must be one of {",".join([move for move in Move])}'
+                )
+
     if tmp_gap:
         max_gap = max(max_gap, tmp_gap)
         min_gap = min(min_gap, tmp_gap) if tmp_gap != 0 else min_gap
         n_gaps += 1
         tmp_gap = None
-        
-    return Alignment(subseq1[::-1], subseq2[::-1], max_gap, min_gap, n_gaps, starting_cell.score, starting_cell.indices)
 
-def printable_matrix(matrix, seq1, seq2):
-    df = pd.DataFrame(matrix, index=[(i, c) for i, c in enumerate(' '+seq1)], columns=[(i, c) for i, c in enumerate(' '+seq2)])
-    
-    return str(df) + '\n\n'
+    return Alignment(
+        subseq1[::-1],
+        subseq2[::-1],
+        max_gap,
+        min_gap,
+        n_gaps,
+        starting_cell.score,
+        starting_cell.indices,
+    )
 
-def find_alignments_by_score(scoring_matrix, seq1, seq2):
+
+def printable_matrix(matrix: List[list], seq1: str, seq2: str) -> str:
+    df = pd.DataFrame(
+        matrix,
+        index=[(i, c) for i, c in enumerate(" " + seq1)],
+        columns=[(i, c) for i, c in enumerate(" " + seq2)],
+    )
+
+    return str(df) + "\n\n"
+
+
+def find_alignments_by_score(scoring_matrix: List[List[Cell]], seq1: str, seq2: str) -> Alignments:
     alignments = Alignments()
-    n, m = len(seq1)+1, len(seq2)+1
+    n, m = len(seq1) + 1, len(seq2) + 1
 
     for i in range(1, n):
         for j in range(1, m):
             cell = scoring_matrix[i][j]
             if cell.score > 0:
                 alignments.append(traceback_process(scoring_matrix, seq1, seq2, cell))
-    
+
     return alignments
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description='Implementation of the Smith and Waterman algorithm for local sequence alignment by Daniele Isoni',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+        description="Implementation of the Smith and Waterman algorithm for local sequence alignment by Daniele Isoni",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
-    parser.add_argument('seq1', type=str, help='First input sequence')
-    parser.add_argument('seq2', type=str, help='Second input sequence')
-    parser.add_argument('--match-score', type=float, default=3.0, help='The score for a sequence match')
-    parser.add_argument('--mismatch-score', type=float, help='The score for a sequence mismatch, it is set to negative MATCH_SCORE if not provided')
-    parser.add_argument('--gap-penalty', type=float, default=-2.0, help='The penalty for a sequence gap')
-    parser.add_argument('--output-file', '-o', type=str, help='Specify file to save the output')
-    parser.add_argument('--improvement', action='store_true', help='Prints all alignments with length > 5, score > 4 and gaps > 0')
+    parser.add_argument("seq1", type=str, help="First input sequence")
+    parser.add_argument("seq2", type=str, help="Second input sequence")
+    parser.add_argument(
+        "--match-score", type=float, default=3.0, help="The score for a sequence match"
+    )
+    parser.add_argument(
+        "--mismatch-score",
+        type=float,
+        help="The score for a sequence mismatch, it is set to negative MATCH_SCORE if not provided",
+    )
+    parser.add_argument(
+        "--gap-penalty", type=float, default=-2.0, help="The penalty for a sequence gap"
+    )
+    parser.add_argument("--output-file", "-o", type=str, help="Specify file to save the output")
+    parser.add_argument(
+        "--improvement",
+        action="store_true",
+        help="Prints all alignments with length > 5, score > 4 and gaps > 0",
+    )
 
     args = parser.parse_args()
 
@@ -145,7 +189,7 @@ if __name__ == '__main__':
     improvement = args.improvement
 
     scoring_matrix = compute_scoring_matrix(seq1, seq2, match_score, mismatch_score, gap_penalty)
-    
+
     max_score_cell = max(chain.from_iterable(scoring_matrix))
 
     best_alignement = traceback_process(scoring_matrix, seq1, seq2, max_score_cell)
@@ -154,13 +198,23 @@ if __name__ == '__main__':
 
     if output_file:
         if os.path.exists(output_file):
-            choice = input(f'The file {output_file} already exists. Do you want to overwrite it? [y/N]\n').lower() or 'n'
-            while choice not in ['y', 'yes', 'n', 'no']:
-                choice = input(f'{Fore.YELLOW}[WARN] {Style.RESET_ALL}Invalid input. Please enter one of the following: [y/N]\n').lower() or 'n'
-            if choice == 'n':
+            choice = (
+                input(
+                    f"The file {output_file} already exists. Do you want to overwrite it? [y/N]\n"
+                ).lower()
+                or "n"
+            )
+            while choice not in ["y", "yes", "n", "no"]:
+                choice = (
+                    input(
+                        f"{Fore.YELLOW}[WARN] {Style.RESET_ALL}Invalid input. Please enter one of the following: [y/N]\n"
+                    ).lower()
+                    or "n"
+                )
+            if choice == "n":
                 sys.exit(0)
 
-        f = open(output_file, 'w')
+        f = open(output_file, "w")
         f.write(matrix_to_print)
 
     print(matrix_to_print)
@@ -169,20 +223,18 @@ if __name__ == '__main__':
         alignments = find_alignments_by_score(scoring_matrix, seq1, seq2)
 
         filtered_alignments = alignments.filter(length__gt=5, score__gt=4, min_gap__gt=0)
-    
-        filtered_alignments.sort(key='score', reverse=True)
+
+        filtered_alignments.sort(key="score", reverse=True)
 
         for al in filtered_alignments:
             if output_file:
-                f.write(al.to_string(to_file=True) + '\n')
+                f.write(al.to_string(to_file=True) + "\n")
             print(al)
     else:
-        out_string = f'''seq1: {seq1}
+        out_string = f"""seq1: {seq1}
 seq2: {seq2}
 {best_alignement}
-'''
+"""
         if output_file:
             f.write(out_string)
         print(out_string)
-        
-
